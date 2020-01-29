24117c53571b1e740409bdbaacf721032e054689
Curtis pachtinger <cpach96@gmail.com>
2020-01-29 05:55:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -9,12 +9,5 @@ def square_array(array)
 end
       return new_array
       
-      new_array.each do |integer|
-        
-        newer_array << integer ** 2 
-        
-      end
-      
-      return newer_array
     
 end
