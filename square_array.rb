bfbe4487aea84601799e817f39fb7e2f2386bc44
Curtis pachtinger <cpach96@gmail.com>
2020-01-29 05:23:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+
+  array_num = [1 , 2 , 3]
+  
+  array_num.each do |square|
+  
+  square ** 2 
+    
+    array_num << square
+end
+
+array_num
+      
+
 end
